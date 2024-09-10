5edc04fe65f943a04e832bed85664a170bf935db
Nuno Araújo <nuno.araujo@itsector.pt>
2024-09-10 20:11:29 +0100

    added .env file

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "b66bf574fbb044bb804d3285152fe61a",
+    key: import.meta.env.VITE_API_KEY,
   },
 });
